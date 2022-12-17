92348a1bb32480f19e72cc4fa7226da5285246a5
gedizzletsplay@gmail.com <Duke1@234>
2022-12-17 21:52:30 +0000

    finish modify seed

--- manager.py
+++ manager.py
@@ -10,7 +10,7 @@ class SeedManager():
         newSeed = Seed(id)
         primary = input("Enter the primary seed genes: ")
         newSeed.primary = primary
-        numGenes = input("How many genes would you like to add to this seed?")
+        numGenes = input("How many genes would you like to add to this seed: ")
         for x in range(1, int(numGenes) + 1):
             neighbor = input("Enter a neighbor gene: ")
             newSeed.neighbors.append(neighbor)
@@ -23,11 +23,36 @@ class SeedManager():
 
     def print_seeds(self):
         for seed in self.seeds:
-            print("----------------")
-            print(f"Seed #{seed.id}")
-            print("----------------")
-            print(f"Primary: {seed.primary}")
-            print("----------------")
-            for i in range(0, len(seed.neighbors)):
-                print(f" Neighbor {i}: {seed.neighbors[i]}")
-            print("-------------------------------")
+            self.print_single_seed(seed)
+        print("\n\n\n")
+            
+
+    def modify_seed(self, seed):
+        print("Seed to modify: ")
+        self.print_single_seed(seed)
+        print("1) Modify primary")
+        print("2) Modify neighbor")
+        print("0) Go back to menu")
+        userInput = input("\nEnter a command: ")
+        if userInput == "1":
+            newPrimary = input("Enter new primary genes: ")
+            seed.primary = newPrimary
+        if userInput == "2":
+            neighbor = input("Which neighbor would you like to edit: ")
+            if seed.neighbors[int(neighbor)]:
+
+                newNeighbor = input("Enter new gene: ")
+                seed.neighbors[int(neighbor)] = newNeighbor
+            else:
+                print("invalid neighbor index")
+
+
+    def print_single_seed(self, seed):
+        print("----------------")
+        print(f"Seed #{seed.id}")
+        print("----------------")
+        print(f"Primary: {seed.primary}")
+        print("----------------")
+        for i in range(0, len(seed.neighbors)):
+            print(f" Neighbor {i}: {seed.neighbors[i]}")
+        print("-------------------------------\n")
